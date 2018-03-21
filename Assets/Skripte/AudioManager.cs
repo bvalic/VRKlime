3d07c2f647f7925fe8273ff0bd02a9b6861539ed
bboyAutistic <35199206+bboyAutistic@users.noreply.github.com>
2018-03-21 01:25:26 +0100

    Audio i TV, zidovi i teleport stepenice
    kao sto ime kaze dodao sam audio,
    pod to mislim na audio manager skriptu koja se moze dodati skup s audio manager prefabom na kojeg bacas videe koji sviraju u pozadini i moguce je prebacivati se iz scene u scenu a da se audio ne gasi
    
    dodao sam novu scenu s TVom koji RADI !!!
    nema gumba na njemu ni nis to cu stura probat sve postelati (bdw. ima i zvuk thats nice)
    
    skino sam one standard ASSestse i potrpao nesto po sceni da se najde da ne hodamo samo bezveze i malo popravio teleport area da pase bolje

---
+++ Assets/Skripte/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public Sound[] sounds;
+
+	public static AudioManager instance;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+			s.source.outputAudioMixerGroup = s.outputMixer;
+
+
+		}
+	}
+
+	void Start()
+	{
+		// Play("sound1");
+	}
+
+
+	public void Play(string nameWeSeek)
+	{
+		Sound s = Array.Find(sounds, sound => sound.nameOfSound == nameWeSeek);
+		if (s == null)
+		{
+			Debug.Log("NO SOUND FOUND!!!");
+			return;
+		}
+
+		foreach (Sound soundToStop in sounds)
+		{
+			soundToStop.source.Stop();
+		}
+		s.source.Play();
+
+
+
+	}
+}
